eff07d5b39416bfd69fa3e495f3cca7da7cd64bc
Krishnan M <km@krishnanm.com>
2023-12-01 21:00:56 +0000

    [wip] day 1: basic functions

--- src/aoc/day1.py
+++ src/aoc/day1.py
@@ -1,2 +1,11 @@
-def combine(first, last):
-    return first + last
+def calibration_value(line):
+    return combine(first_and_last_numbers(line))
+
+
+def first_and_last_numbers(line):
+    numbers = list(filter(lambda x: x.isdigit(), line))
+    return (int(numbers[0]), int(numbers[-1]))
+
+
+def combine(twople):
+    return twople[0] + twople[1]
--- tests/day1_test.py
+++ tests/day1_test.py
@@ -1,4 +1,16 @@
 from aoc import day1
 
+
+def test_calibration_value():
+    line = "1abc2"
+    assert 3 == day1.calibration_value(line)
+
+
+def test_first_and_last_numbers():
+    line = "1abc2"
+    assert (1, 2) == day1.first_and_last_numbers(line)
+
+
+
 def test_combine():
-    assert 3 == day1.combine(1, 2)
+    assert 3 == day1.combine((1, 2))
