43a3006e936874750e6fd8f1989137c93a35e2b1
Krishnan M <km@krishnanm.com>
2023-12-01 21:32:50 +0000

    DONE day 1, problem 1

--- src/aoc/day1.py
+++ src/aoc/day1.py
@@ -1,3 +1,6 @@
+import functools
+
+
 def calibration_value(line):
     return combine(first_and_last_numbers(line))
 
@@ -9,3 +12,9 @@ def first_and_last_numbers(line):
 
 def combine(twople):
     return (twople[0] * 10) + twople[1]
+
+
+if __name__ == "__main__":
+    f = open('src/resources/day1.txt', 'r')
+    sum = functools.reduce(lambda x,y: x + calibration_value(y), f.readlines(), 0)
+    print(f"Day 1, problem 1: sum of calibration values: {sum}")
