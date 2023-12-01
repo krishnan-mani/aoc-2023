e390cf924241b54d9ff1b742cfbba610988c6fdb
Krishnan M <km@krishnanm.com>
2023-12-01 21:13:19 +0000

    [wip] day 1: refactoring

--- tests/day1_test.py
+++ tests/day1_test.py
@@ -2,9 +2,11 @@ from aoc import day1
 import pytest
 
 
-def test_calibration_value():
-    line = "1abc2"
-    assert 3 == day1.calibration_value(line)
+@pytest.mark.parametrize('line,expected', [
+    ("1abc2", 3)
+])
+def test_calibration_value(line, expected):
+    assert expected == day1.calibration_value(line)
 
 
 def test_first_and_last_numbers():
@@ -12,8 +14,8 @@ def test_first_and_last_numbers():
     assert (1, 2) == day1.first_and_last_numbers(line)
 
 
-@pytest.mark.parametrize('twople,result', [
+@pytest.mark.parametrize('twople,expected', [
     ((1, 2), 3),
 ])
-def test_combine(twople, result):
-    assert result == day1.combine(twople)
+def test_combine(twople, expected):
+    assert expected == day1.combine(twople)
